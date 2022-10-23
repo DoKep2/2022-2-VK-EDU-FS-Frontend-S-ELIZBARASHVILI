dca5bf42a7c6cc5bdb6a9b77ab14a4395188935a
Sergo <sergo.elisbar@yandex.ru>
2022-10-23 23:59:52 +0300

    feature/hw2-add-message-sender

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -1,42 +1,86 @@
 import './index.css';
 import './index.html'
+
+const CLASS_ATTRIBUTE_NAME = "class";
+const P_TAG_NAME = "p";
+const CHAT_HISTORY_KEY = "chatHistory";
+const LAST_MESSAGE_SELECTOR = ".dialog-data p:nth-last-child(2)"
 const ex = [
     {
-        "datetime" : "",
+        "datetime": "",
         "name": "me",
         "data": " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto dolorem facilis inventore laboriosam laborum laudantium, mollitia nemo nostrum officia quae qui repellat voluptatum? Ad architecto est, incidunt maxime qui sed. Accusamus consectetur cumque cupiditate debitis dignissimos facere id in minus necessitatibus, nobis odit quos repellendus sint tempora, temporibus. Deserunt!"
     },
     {
-        "datetime" : "",
+        "datetime": "",
         "name": "user",
         "data": "sadlsadl zc zxklz klxczx c"
     },
     {
-        "datetime" : "",
+        "datetime": "",
         "name": "user",
         "data": "lorem123 aseqwe "
     }
 ]
 
-// Каждому пользаку сделать айди и брать историю сообщений по итему <id>_chat_history
-localStorage.setItem("chatHistory", JSON.stringify(ex));
-const history = JSON.parse(localStorage.getItem("chatHistory"))
-history.forEach(message => console.log(message));
-const dialogHeader = document.getElementsByClassName("dialog-header")[0]
-const newDiv = document.createElement("p")
-newDiv.setAttribute("class", "user")
-const newContent = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus error eveniet illo praesentium quas sapiente vel! Consequuntur et repellat repellendus.");
-newDiv.appendChild(newContent)
-dialogHeader.parentNode.insertBefore(newDiv, dialogHeader.nextSibling)
+
+const form = document.querySelector('form');
+const input = document.querySelector('.form-input');
+
+form.addEventListener('submit', handleSubmit.bind(this));
+form.addEventListener('keypress', handleKeyPress.bind(this));
+
+function handleSubmit(event) {
+    event.preventDefault();
+    if(input.value === "") return;
+    sendMessage(input.value, "chatHistory");
+    reloadMessages("chatHistory");
+    input.value = ""
+}
+
+function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+        form.dispatchEvent(new Event('submit'));
+    }
+}
 
 
-function showMessages(){
+//todo Каждому пользаку сделать айди и брать историю сообщений по userID
+function setExampleMessages() {
+    if(!localStorage.getItem(CHAT_HISTORY_KEY))
+        localStorage.setItem(CHAT_HISTORY_KEY, JSON.stringify(ex));
+}
+
+function reloadMessages(localStorageKey){
+    while(document.getElementsByClassName("me").length > 0){
+        document.getElementsByClassName("me")[0].remove();
+    }
+    while(document.getElementsByClassName("user").length > 0){
+        document.getElementsByClassName("user")[0].remove();
+    }
+    showMessages(localStorageKey);
+}
+
+function showMessages(localStorageKey) {
+    const history = JSON.parse(localStorage.getItem(localStorageKey));
+    let lastMessage = document.querySelector(LAST_MESSAGE_SELECTOR);
+    if (lastMessage == null) {
+        lastMessage = document.querySelector(".dialog-header")
+    }
     history.forEach(h => {
-        const newDiv = document.createElement("p");
-        newDiv.setAttribute("class", h.name);
+        const newDiv = document.createElement(P_TAG_NAME);
+        newDiv.setAttribute(CLASS_ATTRIBUTE_NAME, h.name);
         newDiv.appendChild(document.createTextNode(h.data));
-        dialogHeader.parentNode.insertBefore(newDiv, dialogHeader.nextSibling);
+        lastMessage.parentNode.insertBefore(newDiv, lastMessage.nextSibling);
     })
 }
 
-showMessages()
+function sendMessage(text, localStorageKey) {
+    const currentHistory = JSON.parse(localStorage.getItem(localStorageKey));
+    // todo:
+    const message = JSON.parse(`{"datetime":"${Date.now()}", "name":"me", "data":"${text}"}`);
+    currentHistory.push(message);
+    localStorage.setItem(localStorageKey, JSON.stringify(currentHistory));
+}
+showMessages("chatHistory")
+
